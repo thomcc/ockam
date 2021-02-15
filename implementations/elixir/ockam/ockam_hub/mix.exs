9e5e205eea42ae47d4ea3a856e88d69846e19749
Michael J. Cohen <mika@ockam.io>
2021-02-15 16:34:36 -0700

    fix(elixir): migrate ockam_hub from old ockam path

--- implementations/elixir/ockam/ockam_hub/mix.exs
+++ implementations/elixir/ockam/ockam_hub/mix.exs
@@ -45,8 +45,9 @@ defmodule Ockam.Hub.MixProject do
 
   defp deps do
     [
-      {:ockam, path: "../_ockam"},
+      {:ockam, path: "../ockam"},
       {:ockam_vault_software, path: "../ockam_vault_software"},
+      {:ranch, "~> 2.0"},
       {:telemetry, "~> 0.4.2"},
       {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
